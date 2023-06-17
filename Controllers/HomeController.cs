692fafc5e126ca073f4765df22f07a4044070476
Jakub Zięba <jakubzieba7@gmail.com>
2023-06-17 11:19:12 +0200

    Improving protection entries

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -94,6 +94,7 @@ namespace InvoiceManager.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Invoice(Invoice invoice)
         {
             var userId = User.Identity.GetUserId();
@@ -114,6 +115,7 @@ namespace InvoiceManager.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult InvoicePosition(InvoicePosition invoicePosition)
         {
             var userId = User.Identity.GetUserId();
