7d933725869ddbba4ddc4cc6df44ca15cb8e5079
Jakub Zięba <jakubzieba7@gmail.com>
2023-06-17 10:18:25 +0200

    Rotativa NuGet and Ajax generatePdf function

--- Controllers/PrintController.cs
+++ Controllers/PrintController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using InvoiceManager.Models.Domains;
+using InvoiceManager.Models.Repositories;
+using Microsoft.AspNet.Identity;
+using Rotativa;
+using Rotativa.Options;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +17,51 @@ namespace InvoiceManager.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        private InvoiceRepository _invoiceRepository = new InvoiceRepository();
+
+        public ActionResult InvoiceToPdf(int id)
+        {
+            var handle = Guid.NewGuid().ToString();
+            var userId = User.Identity.GetUserId();
+            var invoice = _invoiceRepository.GetInvoice(id, userId);
+
+            TempData[handle] = GetPdfContent(invoice);
+
+            return Json(new
+            {
+                FileGuid = handle,
+                FileName = $@"Faktura_{invoice.ID}.pdf"
+            });
+        }
+
+        private byte[] GetPdfContent(Invoice invoice)
+        {
+            var pdfResult = new ViewAsPdf(@"InvoiceTemplate", invoice)
+            {
+                PageSize = Size.A4,
+                PageOrientation = Orientation.Portrait
+            };
+
+            return pdfResult.BuildFile(ControllerContext);
+        }
+
+        public ActionResult DownloadInvoicePdf(string fileGuid, string fileName)
+        {
+            if (TempData[fileGuid] == null)
+                throw new Exception("Błąd przy próbie eksportu faktury do PDF.");
+
+            var data = TempData[fileGuid] as byte[];
+            return File(data, "application/pdf", fileName);
+        }
+
+        public ActionResult InvoiceTemplate(int id)
+        {
+            var userId = User.Identity.GetUserId();
+            var invoice = _invoiceRepository.GetInvoice(id, userId);
+
+            return View(invoice);
         }
     }
 }
